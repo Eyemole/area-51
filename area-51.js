28c1e6164adf6878426101e28ea819a66bd18d56
Yana Pertels <ypertels@gmail.com>
2017-09-09 12:04:24 -0700

    some documentation

--- area-51.js
+++ area-51.js
@@ -18,7 +18,7 @@ window.onload = function() {
     var formel = document.getElementsByTagName('form')[0];
   	console.log(formel);
 
-    // When the Session ID is entered into the form, run the getsocket function
+    // When the Session ID is entered into the form, run the getsocket() function
 
     if (formel.addEventListener) {
       formel.addEventListener("submit", function(evt) { evt.preventDefault(); getsocket(); return false; }, false);
@@ -28,11 +28,17 @@ window.onload = function() {
 
     function getsocket() {
       
+    // Get the session ID value 
     sessionid = formel.elements[0].value;
     console.log("https://eeg-connector.herokuapp.com/" + sessionid);
+
+    //The EEG/GSR data from the connector app is sent to the eeg-connector server 
+    //to the {sessionid} room. The line below connects to that room 
     var socket = io("https://eeg-connector.herokuapp.com/" + sessionid).connect();
     console.log(socket);
 
+
+    //Move one of the 4 spheres 
     function moveY(el, data) {
       var pos = el.getAttribute("position");
       var anim = el.getElementsByTagName('a-animation')[0];
@@ -42,19 +48,30 @@ window.onload = function() {
       el.setAttribute("position", pos);
     }
 
+    //Change the colour of the sky according to the GSR data
     function changeSkyColour(data) {
-      data = Math.min(EEG_MAX, data);
+
+      data = Math.min(GSR_MAX, data);
       var sky = document.getElementsByTagName('a-sky')[0];
-      var r = Math.floor(255 * data / EEG_MAX);
+
+      //Amount of red in the sky is inversely proportional to the GSR value 
+      //Low arousal = blue sky, high arousal = red sky 
+      var r = Math.floor(255 * (1 - data / GSR_MAX));
       sky.setAttribute("color", "rgb(" + r + "," + G + "," + B + ")");;
     }
 
+
+    //Listen to messages from the eeg-connector server.
+    //On receiveng a "out/muse/eeg/{i}" message, 
+    //Move the i'th sphere
     for (var i = 0; i < ADDRESSES[EEG_ADDR]; i++) {
       socket.on(EEG_ADDR + i, function(data) {
           moveY(document.getElementById(CHANNEL_MAP[i]), data);
     });
     }
 
+    //On receiveng a "out/gsr" message, 
+    //Update the sky colour
     socket.on(GSR_ADDR + 0, function(data) {
           changeSkyColour(data);
     });
