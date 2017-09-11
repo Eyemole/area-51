5cbab62adf81c27f5d1e96f8392f0ece841e3f4d
Yana Pertels <ypertels@gmail.com>
2017-09-11 12:45:40 -0700

    ocean changing with alpha

--- area-51.js
+++ area-51.js
@@ -4,13 +4,21 @@ const EEG_RANGE = EEG_MAX - EEG_MIN;
 const MAX_HEIGHT = 5;
 const GSR_MIN = 200;
 const GSR_MAX = 500;
+const ALPHA_MIN = 0.01;
+const ALPHA_MAX = 2;
+const MAX_OCEAN_SPEED = 10;
+const MIN_OCEAN_SPEED = 0.1;
 const B = 172;
 const G = 113;
 
 const EEG_ADDR = "out/muse/eeg/";
+const ALPHA_ADDR = "out/muse/elements/alpha_absolute";
+const BLINK_ADDR = "out/muse/elements/blink";
 const GSR_ADDR = "out/gsr/";
-var ADDRESSES = {"out/muse/eeg/": 4, "out/gsr/": 1};
+var ADDRESSES = {"out/muse/eeg/": 4, "out/muse/elements/alpha_absolute": 4, "out/muse/elements/beta_absolute": 4,
+ "out/muse/elements/theta_absolute": 4, "out/muse/elements/blink": 1, "out/gsr/": 1};
 var CHANNEL_MAP = {0: "tp9", 1: "af7", 2: "af8", 3: "tp10"};
+var alphas = [0, 0, 0, 0];
 
 window.onload = function() {
 
@@ -47,6 +55,14 @@ window.onload = function() {
       anim.setAttribute("to", pos);
     }
 
+
+    //Turn off the sky when you're blinking
+    function blackenSky() {
+
+      var sky = document.getElementsByTagName('a-sky')[0];
+      sky.setAttribute("color", "#000");
+    }
+
     //Change the colour of the sky according to the GSR data
     function changeSkyColour(data) {
 
@@ -60,6 +76,20 @@ window.onload = function() {
     }
 
 
+    //Change the speed of the ocean according to absolute alpha values
+    function changeOceanSpeed(data) {
+
+      var ocean = document.getElementsByTagName('a-sky')[0];
+      ocean.setAttribute("speed", data / (ALPHA_MAX - ALPHA_MIN) * MAX_OCEAN_SPEED)
+
+    }
+
+    function updateAlpha(data, id) {
+
+      data = Math.max(Math.min(ALPHA_MAX, data), ALPHA_MIN);
+      alphas[id] = data;
+    }
+
     //Listen to messages from the eeg-connector server.
     //On receiveng a "out/muse/eeg/{i}" message, 
     //Move the i'th sphere
@@ -70,10 +100,28 @@ window.onload = function() {
     });
     }
 
+    //Listen to messages from the eeg-connector server.
+    //On receiveng a "out/muse/elements/alpha_absolute/{i}" message, 
+    //Change the speed of the ocean 
+
+    for (let i = 0; i < ADDRESSES[ALPHA_ADDR]; i++) {
+      socket.on(ALPHA_ADDR + i, function(data) {
+          updateAlpha(data, i);
+          changeOceanSpeed(alphas.reduce((a,b) => (a+b)) / alphas.length;)
+    });
+    }
+
     //On receiveng a "out/gsr" message, 
     //Update the sky colour
     socket.on(GSR_ADDR + 0, function(data) {
           changeSkyColour(data);
+    });
+
+
+    //On receiveng a "out/muse/elements/blink/0" message, 
+    //Just turn off the sky
+    socket.on(BLINK_ADDR + 0, function(data) {
+          blackenSky();
     });
 
   }
