7cd90323a9ee250fdbaeb7a9d1f214d9390d0087
Yana Pertels <ypertels@gmail.com>
2017-09-10 21:57:43 -0700

    .

--- area-51.js
+++ area-51.js
@@ -8,7 +8,7 @@ const G = 113;
 
 const EEG_ADDR = "out/muse/eeg/";
 const GSR_ADDR = "out/gsr/";
-var ADDRESSES = {EEG_ADDR: 4, GSR_ADDR: 1};
+var ADDRESSES = {"out/muse/eeg/": 4, "out/gsr/": 1};
 var CHANNEL_MAP = {0: "tp9", 1: "af7", 2: "af8", 3: "tp10"};
 
 window.onload = function() {
